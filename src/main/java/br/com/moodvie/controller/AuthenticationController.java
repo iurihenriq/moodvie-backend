a1069285b0d4e94ca7b00c7297d63021d5bbf31b
Davi Pinheiro <davipinheiro935@gmail.com>
2023-05-01 14:48:41 -0300

    adding hello world endpoint for testing

--- src/main/java/br/com/moodvie/controller/AuthenticationController.java
+++ src/main/java/br/com/moodvie/controller/AuthenticationController.java
@@ -8,10 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
@@ -24,6 +21,11 @@ public class AuthenticationController {
         Authentication authentication = manager.authenticate(token);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping()
+    public String helloWorld(){
+        return "Hello World!";
     }
 
 }
